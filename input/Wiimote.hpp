0d8e82c03f04916e648c2ec4f4aee3e4aad39aa4
David M. Anderson <dma@epsilondelta.us>
2009-09-04 04:05:31 -0700

    Add comments about Wiimote.

--- input/Wiimote.hpp
+++ input/Wiimote.hpp
@@ -4,7 +4,19 @@
   Wiimote.hpp
   Copyright (C) 2009 David M. Anderson
 
-  Wiimote class: a standard wiimote, a.k.a. joystick.
+  Wiimote class: a (Nintendo) Wii remote, or the equivalent.
+  NOTES:
+  1. The number of buttons, axes, and accelerometers depends on whether the
+     nunchuk is attached.
+  2. With the Wiimote horizontal, buttons up, pointing the sensor forward
+     (toward the LEDs near the TV), the accelerometer axes are as follows:
+     Positive X is acceleraton rightward.
+     Positive Y is acceleration downward.
+     Positive Z is acceleration rearward.
+     These match Nintendo's KPAD conventions.
+  3. Gravity() reports the direction of the force opposing gravity as estimated
+     when the Wiimote is relatively still. So with the Wiimote oriented as 
+     above (2), the gravity vector will be approximately (0, -1, 0).
 */
 
 

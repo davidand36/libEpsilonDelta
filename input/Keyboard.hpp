8252d0ee66a086fb3989fde3b31a8b88ea1b7cf6
David M. Anderson <dma@epsilondelta.us>
2009-09-18 19:00:36 -0700

    Add Keyboard enum for keys.

--- input/Keyboard.hpp
+++ input/Keyboard.hpp
@@ -9,6 +9,9 @@
 
 
 #include "InputDevice.hpp"
+#ifdef USE_SDL
+#include <SDL_keysym.h>
+#endif
 
 
 namespace EpsilonDelta
@@ -32,6 +35,56 @@ public:
 
     virtual int NumButtons( ) const;
     virtual bool ButtonDown( int button ) const;
+
+#ifdef USE_SDL
+    enum Key
+    {
+        Left        = SDLK_LEFT,
+        Right       = SDLK_RIGHT,
+        Up          = SDLK_UP,
+        Down        = SDLK_DOWN,
+        Home        = SDLK_HOME,
+        End         = SDLK_END,
+        PageUp      = SDLK_PAGEUP,
+        PageDown    = SDLK_PAGEDOWN,
+        Insert      = SDLK_INSERT,
+        Delete      = SDLK_DELETE,
+        Backspace   = SDLK_BACKSPACE,
+        Return      = SDLK_RETURN,
+        Enter       = SDLK_KP_ENTER,
+        Tab         = SDLK_TAB,
+        Escape      = SDLK_ESCAPE,
+        F1          = SDLK_F1,
+        F2          = SDLK_F2,
+        F3          = SDLK_F3,
+        F4          = SDLK_F4,
+        F5          = SDLK_F5,
+        F6          = SDLK_F6,
+        F7          = SDLK_F7,
+        F8          = SDLK_F8,
+        F9          = SDLK_F9,
+        F10         = SDLK_F10,
+        F11         = SDLK_F11,
+        F12         = SDLK_F12,
+        CapsLock    = SDLK_CAPSLOCK,
+        NumLock     = SDLK_NUMLOCK,
+        ScrollLock  = SDLK_SCROLLOCK,
+        LeftShift   = SDLK_LSHIFT,
+        RightShift  = SDLK_RSHIFT,
+        LeftCtrl    = SDLK_LCTRL,
+        RightCtrl   = SDLK_RCTRL,
+        LeftAlt     = SDLK_LALT,
+        RightAlt    = SDLK_RALT,
+        LeftMeta    = SDLK_LMETA,
+        RightMeta   = SDLK_RMETA,
+        LeftSuper   = SDLK_LSUPER,
+        RightSuper  = SDLK_RSUPER,
+        PrintScrn   = SDLK_PRINT,
+        SysReq      = SDLK_SYSREQ,
+        Break       = SDLK_BREAK,
+        Menu        = SDLK_MENU
+    };
+#endif //SDL
 
 private:
     shared_ptr< KeyboardImpl >  m_pImpl;
